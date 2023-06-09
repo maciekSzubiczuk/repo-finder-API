63282f1f1f48e218def0242e67c935387ca9e85b
Maciej Szubiczuk <maciej.szubiczuk1@gmail.com>
2023-06-09 13:14:42 +0200

    Created basic project structure

---
+++ src/main/java/com/example/repofinderapi/RepoFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.repofinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoFinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoFinderApiApplication.class, args);
+    }
+
+}
