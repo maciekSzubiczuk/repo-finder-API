e1b0f85a69372cdf36ffd9f87e3a65e1fc542a7a
Maciej Szubiczuk <maciej.szubiczuk1@gmail.com>
2023-06-10 13:53:24 +0200

    Added performing a get request to list branches API

--- src/main/java/com/example/repofinderapi/controller/RepoController.java
+++ src/main/java/com/example/repofinderapi/controller/RepoController.java
@@ -1,5 +1,6 @@
 package com.example.repofinderapi.controller;
 
+import com.example.repofinderapi.service.BranchesService;
 import com.example.repofinderapi.service.ReposService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +16,21 @@ import java.util.List;
 public class RepoController {
 
     private final ReposService reposService;
+    private final BranchesService branchesService;
 
     @Autowired
-    RepoController(ReposService reposService){
+    RepoController(ReposService reposService, BranchesService branchesService){
         this.reposService = reposService;
+        this.branchesService = branchesService;
     }
 
     @GetMapping("/{username}")
     public ResponseEntity<String> getUserRepos(@PathVariable String username) {
         List<HashMap<String, Object>> reposResponse = reposService.getReposResponse(username);
+        List<HashMap<String, Object>> branchesResponse = branchesService.getBranchesResponse(username,"repo-finder-API");
         System.out.println(reposResponse);
+        System.out.println();
+        System.out.println(branchesResponse);
         return ResponseEntity.ok("Requested username: "+username);
     }
 
